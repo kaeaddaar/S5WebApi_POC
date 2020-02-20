285501314cb1882d54a005280205f858eeddc012
kaeaddaar <playinfc@hotmail.ca>
2020-02-20 13:38:03 -0800

    Init

---
+++ APBills/APBills/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
